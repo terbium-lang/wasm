fa27401e124a8b1f56dd8fdb5e9358a2c454e5a1
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-06-20 14:29:01 -0700

    Use macro to avoid boilerplating

--- src/lib.rs
+++ src/lib.rs
@@ -1,82 +1,97 @@
-use terbium::{AstBody, AstNode, AstParseInterface, BcTransformer, DefaultInterpreter};
+use terbium::{AstBody, AstNode, AstParseInterface, BcTransformer, DefaultInterpreter, BcProgram};
 use wasm_bindgen::prelude::*;
 
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+macro_rules! handle_output {
+    ($output:ident, $errors:ident) => {{
+        if !$errors.is_empty() {
+            return format!(
+                "{}\nErrors: {:?}",
+                $output,
+                $errors
+                    .into_iter()
+                    .map(|e| format!("{:?}\n", e))
+                    .collect::<String>()
+            )
+            .into();
+        }
+
+        $output.into()
+    }};
+    ($output:ident, $errors:ident, $errfmt:literal, $outputfmt:literal) => {{
+        if !$errors.is_empty() {
+            return format!(
+                $errfmt,
+                $output,
+                $errors
+                    .into_iter()
+                    .map(|e| format!("{:?}\n", e))
+                    .collect::<String>()
+            )
+            .into();
+        }
+
+        format!($outputfmt, $output).into()
+    }};
+}
+
+macro_rules! ast {
+    ($content:ident, $asttype:ty) => {{
+        match <$asttype>::from_string($content) {
+            Ok(t) => t,
+            Err(e) => {
+                return e
+                    .iter()
+                    .map(|e| format!("[{:?}]{}\n", e.span, e.message))
+                    .collect::<String>()
+                    .into();
+            }
+        }
+    }};
+}
+
+macro_rules! program {
+    ($body:ident) => {{
+        let mut transformer = BcTransformer::new();
+        transformer.interpret_body(None, $body);
+    
+        let mut program = transformer.program();
+        program.resolve();
+
+        program
+    }}
+}
+
 #[must_use]
 #[wasm_bindgen]
 pub fn ast(content: String) -> JsValue {
-    let (node, errors) = match AstNode::from_string(content) {
-        Ok(t) => t,
-        Err(e) => {
-            return e
-                .iter()
-                .map(|e| format!("[{:?}]{}\n", e.span, e.message))
-                .collect::<String>()
-                .into();
-        }
-    };
+    let (node, errors) = ast!(content, AstNode);
 
-    if !errors.is_empty() {
-        return format!("{:#?}\nErrors: {:?}", node, errors).into();
-    }
-
-    format!("{:#?}", node).into()
+    handle_output!(node, errors, "{:#?}\nErrors: {:?}", "{:#?}")
 }
 
 #[must_use]
 #[wasm_bindgen]
 pub fn dis(code: String) -> JsValue {
-    let (body, errors) = match AstBody::from_string(code) {
-        Ok(t) => t,
-        Err(e) => {
-            return e
-                .iter()
-                .map(|e| format!("[{:?}]{}\n", e.span, e.message))
-                .collect::<String>()
-                .into();
-        }
-    };
-
-    let mut transformer = BcTransformer::new();
-    transformer.interpret_body(None, body);
-
-    let mut program = transformer.program();
-    program.resolve();
+    let (body, errors) = ast!(code, AstBody);
+    let program = program!(body);
 
     let mut output = Vec::new();
     drop(program.dis(&mut output));
 
     let final_output = String::from_utf8(output).unwrap_or_else(|_| unreachable!());
 
-    if !errors.is_empty() {
-        return format!("{}\nErrors: {:?}", final_output, errors).into();
-    }
-
-    final_output.into()
+    handle_output!(final_output, errors)
 }
 
 #[must_use]
 #[wasm_bindgen]
 pub fn interpret(code: String) -> JsValue {
-    let (body, errors) = match AstBody::from_string(code) {
-        Ok(t) => t,
-        Err(e) => {
-            return e
-                .iter()
-                .map(|e| format!("[{:?}]{}\n", e.span, e.message))
-                .collect::<String>()
-                .into();
-        }
-    };
-
-    let mut transformer = BcTransformer::new();
-    transformer.interpret_body(None, body);
-
-    let mut program = transformer.program();
-    program.resolve();
+    let (body, errors) = ast!(code, AstBody);
+    let program = program!(body);
 
     let mut interpreter = DefaultInterpreter::new();
     interpreter.run_bytecode(program);
@@ -85,9 +100,5 @@ pub fn interpret(code: String) -> JsValue {
     let popped = interpreter.ctx.store.resolve(popped);
     let output = interpreter.get_object_repr(popped);
 
-    if !errors.is_empty() {
-        return format!("{}\nErrors: {:?}", output, errors).into();
-    }
-
-    output.into()
+    handle_output!(output, errors)
 }
